f217631189adaa03e173f0857cdf63053fa11e62
Jason Antwi-Appah <hey@jasonaa.me>
2022-09-18 21:26:57 -0500

    feat: initial implementation (untested)
    this theoretically would work how i want it to, but i’m in a cs building and can’t find an accessible tv to test with :(

--- Peppermint/main.swift
+++ Peppermint/main.swift
@@ -5,7 +5,53 @@
 //  Created by Jason Antwi-Appah on 9/18/22.
 //
 
-import Foundation
+// ✨ SET THIS ✨
+let displays: UInt32 = 0
+// ✨ DON'T TOUCH BELOW HERE :)
 
-print("Hello, World!")
+import Foundation
+import CaffeineKit
+
+let bytesPointer = UnsafeMutableRawPointer.allocate(byteCount: 2, alignment: 1)
+bytesPointer.storeBytes(of: 0xFF_FF, as: UInt16.self)
+
+
+let caf = Caffeination(withOpts: [.idle, .display])
+
+var coffee = false {
+	didSet {
+		if coffee {
+			do {
+				let c = try caf.closure {
+					(bruh: Int) -> Void in
+					while coffee {}
+					return
+				}
+				c(1)
+			} catch {
+				print("Bruh")
+			}
+		}
+	}
+}
+
+CGDisplayRegisterReconfigurationCallback({
+	(display: CGDirectDisplayID, flags: CGDisplayChangeSummaryFlags, _: UnsafeMutableRawPointer?) -> Void in print(display, flags)
+	if displays == display {
+		switch (flags) {
+			case .addFlag:
+				coffee = true
+			case .removeFlag:
+				coffee = false
+			default:
+				print("yea")
+		}
+	}
+}, bytesPointer)
+
+
+while true {
+	
+}
+
 
